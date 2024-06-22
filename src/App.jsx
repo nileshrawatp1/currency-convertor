45f2f3167f65950a72c6186cd892fa9b900f12aa
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2024-06-22 20:28:10 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,20 @@ function App() {
     setConvertedAmount(amount * currencyInfo[to]);
   }
 
+  const swapValues = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
   return (
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url('https://images.pexels.com/photos/8369837/pexels-photo-8369837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}
     >
       <div className="w-full">
-        
+
         <h1 className='text-4xl text-center font-bold text-gray-700 my-4'>Currency Convertor</h1>
         <div className="w-full bg-green-600 max-w-xl mx-auto  border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form onSubmit={(e) => {
@@ -42,6 +49,7 @@ function App() {
               <button
                 type="button"
                 className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-700 text-white px-2 scroll-py-0.5'
+                onClick={swapValues}
               >
                 SWAP
               </button>
